e4bd466e8e12dc22c180a90230b11a28e38e245b
Chris Kimpton <chris@kimptoc.net>
2011-06-26 18:56:08 +0100

    init

---
+++ test/dummy/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Dummy::Application.config.secret_token = '0079db6a0c1666ba70a17f3afa62571c81c6305618305553e7aeb1cc3b7df154f55184162b2199e755487e247705e2e9936637cad8948369209c3633ab382893'
---
+++ test/imagesite_test.rb
@@ -0,0 +1,7 @@
+require 'test_helper'
+
+class ImagesiteTest < ActiveSupport::TestCase
+  test "truth" do
+    assert_kind_of Module, Imagesite
+  end
+end
